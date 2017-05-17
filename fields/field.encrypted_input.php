f0d882648c8d35cdc11faf4ffaf78080c6867561
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-05-17 13:38:52 -0400

    Fix compatibility warnings for PHP7

--- fields/field.encrypted_input.php
+++ fields/field.encrypted_input.php
@@ -1,14 +1,14 @@
 <?php
-	
+
 	Class FieldEncrypted_Input extends Field {
-		
+
 		function __construct(){
 			parent::__construct();
 			$this->_name = 'Encrypted Input';
 			$this->_required = TRUE;
 			$this->set('required', 'yes');
 		}
-		
+
 		public function commit(){
 			if(!parent::commit()) return false;
 
@@ -21,20 +21,20 @@
 			Symphony::Database()->query("DELETE FROM `tbl_fields_".$this->handle()."` WHERE `field_id` = '$id' LIMIT 1");
 			return Symphony::Database()->insert($fields, 'tbl_fields_' . $this->handle());
 		}
-		
-		public function displaySettingsPanel(&$wrapper, $errors = null) {
+
+		public function displaySettingsPanel(XMLElement &$wrapper, $errors = null) {
 			parent::displaySettingsPanel($wrapper, $errors);
-			
+
 			$div = new XMLElement('div', NULL, array('class' => 'compact'));
 			$this->appendRequiredCheckbox($div);
 			$wrapper->appendChild($div);
 		}
-		
-		public function displayPublishPanel(&$wrapper, $data=NULL, $flagWithError=NULL, $fieldnamePrefix=NULL, $fieldnamePostfix=NULL, $entry_id = null){
-		    
+
+		public function displayPublishPanel(XMLElement &$wrapper, $data=NULL, $flagWithError=NULL, $fieldnamePrefix=NULL, $fieldnamePostfix=NULL, $entry_id = null){
+
 			$value = General::sanitize($data['value']);
 			$label = Widget::Label($this->get('label'));
-			
+
 			if(empty($value)) {
 			    if($this->get('required') != 'yes') $label->appendChild(new XMLElement('i', __('Optional')));
 			    $label->appendChild(Widget::Input('fields'.$fieldnamePrefix.'['.$this->get('element_name').']'.$fieldnamePostfix, (strlen($value) != 0 ? $value : NULL)));
@@ -49,18 +49,18 @@
 			    $label->appendChild(Widget::Input('fields'.$fieldnamePrefix.'['.$this->get('element_name').']'.$fieldnamePostfix, 'encrypted:' . $data['value'], 'hidden'));
 			    $wrapper->appendChild($label);
 			}
-			
+
 		}
-		
-		function appendFormattedElement(&$wrapper, $data, $encode=FALSE, $mode=NULL, $entry_id=NULL){
+
+		function appendFormattedElement(XMLElement &$wrapper, $data, $encode=FALSE, $mode=NULL, $entry_id=NULL){
 			if(!is_array($data) || empty($data['value'])) return;
-			
+
 			$value = $this->decrypt($data['value']);
-			
+
 			$xml = new XMLElement($this->get('element_name'), General::sanitize($value));
 			$wrapper->appendChild($xml);
 		}
-		
+
 		public function checkPostFieldData($data, &$message, $entry_id=NULL){
 			$message = NULL;
 
@@ -74,10 +74,10 @@
 
 		public function processRawFieldData($data, &$status, &$message=null, $simulate=false, $entry_id=null) {
 			$status = self::__OK__;
-			
+
 			// store empty (NULL) value without encryption if the field is optional
 			if(empty($data)) return array('value' => '');
-			
+
 			// has already been encrypted
 			if(preg_match("/^encrypted:/", $data)) {
 			    return array(
@@ -89,10 +89,10 @@
     				'value' => $this->encrypt($data),
     			);
 			}
-			
+
 		}
-		
-		function encrypt($string) { 
+
+		function encrypt($string) {
 			return trim(
 				base64_encode(
 					mcrypt_encrypt(
@@ -106,16 +106,16 @@
 						)
 					)
 				)
-			); 
-		} 
+			);
+		}
 
-		function decrypt($string) { 
+		function decrypt($string) {
 			return trim(
 				mcrypt_decrypt(
-					MCRYPT_RIJNDAEL_256, 
+					MCRYPT_RIJNDAEL_256,
 					hash('sha256', Symphony::Configuration()->get('salt', 'encrypted_input'), TRUE),
-					base64_decode($string), 
-					MCRYPT_MODE_ECB, 
+					base64_decode($string),
+					MCRYPT_MODE_ECB,
 					mcrypt_create_iv(
 						mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB),
 						MCRYPT_RAND
